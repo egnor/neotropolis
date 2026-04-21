cb95baa9737c61a20c9a8bb4d456bbdb338b8be8
Dan Egnor <egnor@ofb.net>
2026-04-21 10:08:48 -0700

    better system setup

--- trashbot/system_setup_cli.py
+++ trashbot/system_setup_cli.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python3
-"""Machine setup for trashbot/trashbase Pi"""
+"""Machine setup for trashbot/trashbase/test computers"""
 
 import asyncclick as click
 import logging
 import ok_logging_setup
 import ok_subprocess_defaults
-import pathlib
 import platform
 import re
 import textwrap
+from pathlib import Path
 
 
-REPO_PATH = pathlib.Path(__file__).parent.parent
+REPO_PATH = Path(__file__).parent.parent
 
 
 @click.command()
 @click.option("--bot", is_flag=True)
 @click.option("--base", is_flag=True)
-@click.option("--testbed", is_flag=True)
-def main(bot, base, testbed):
+@click.option("--desk", is_flag=True)
+def main(bot, base, desk):
     ok_logging_setup.install()
     sub = ok_subprocess_defaults.SubprocessDefaults()
-    reboot_required = False
 
-    if (bot + base + testbed) > 1:
-        raise click.ClickException("Set only one of --bot, --base, --testbed")
-    if not (bot or base or testbed):
+    if (bot + base + desk) > 1:
+        raise click.ClickException("Set only one of --bot, --base, --desk")
+    if not (bot or base or desk):
         node = platform.node().split(".")[0]
         bot, base = (node == "trashbot"), (node == "trashbase")
         if not (bot or base):
-            raise click.ClickException("Set one of --bot, --base, --testbed")
+            raise click.ClickException("Set one of --bot, --base, --desk")
+
+    reboot_required = False
 
     #
     # apt packages
     #
 
-    apt_needed = ["build-essential"]
-    apt_needed.extend(["libusb-1.0-0-dev", "libhidapi-libusb0"] if base else [])
+    apt_needed = ["build-essential", "libusb-1.0-0-dev", "libhidapi-libusb0"]
+    udev_restart_required = False
 
     logging.info("🎁 Checking apt packages...")
     apt_query = ["dpkg-query", "--show", "--showformat=${Package}\\n"]
@@ -48,7 +49,7 @@ def main(bot, base, testbed):
     #
 
     if bot or base:
-        boot_line_path = pathlib.Path("/boot/firmware/cmdline.txt")
+        boot_line_path = Path("/boot/firmware/cmdline.txt")
         logging.info("🥾 Checking boot args: %s", boot_line_path)
         boot_line = boot_line_path.read_text().strip()
         arg_rx = re.compile(r'([\w.-]+(=("[^"]*")+|=[^"\s]*)?(?![^\s]))\s*|(.)')
@@ -69,50 +70,40 @@ def main(bot, base, testbed):
             reboot_required = True
 
     #
-    # screen layout config via Kanshi
-    # https://gitlab.freedesktop.org/emersion/kanshi
-    # Kanshi is started by default on Pi OS (/etc/xdg/labwc/autostart)
-    #
-
-    kanshi_config = None
-    if bot:
-        # Place displays in a consistent position
-        # Note, both displays are forced on with "...@30e" kernel modes above
-        serials = [
-            "JZI Beetronics bee0097-73092",
-            "JZI BEETRONICS bee0097-63090",
-        ]
-        kanshi_config = textwrap.dedent(f"""
-            profile {{
-                output "{serials[0]}" position 0,0
-                output "{serials[1]}" position 1024,0
-            }}
-        """).lstrip()
-
-    if kanshi_config:
-        kanshi_config_path = pathlib.Path.home() / ".config/kanshi/config"
-        kanshi_config_path.parent.mkdir(parents=True, exist_ok=True)
-        logging.info("🖥️ Setting screen layout: %s", kanshi_config_path)
-        kanshi_config_path.write_text(kanshi_config)
-
-    #
     # sundry system config files
     #
 
     config_files = {
-        "/etc/udev/rules.d/10-streamdeck.rules": (
+        Path("/etc/udev/rules.d/10-streamdeck.rules"): (
             'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0fd9", '
             'GROUP="users", TAG+="uaccess"\n'
         )
     }
 
-    udev_restart_required = False
-    for path_str, contents in config_files.items():
-        path = pathlib.Path(path_str)
-        logging.info("⚙️ Checking %s", path_str)
+    if bot:
+        # Kanshi is started by default on Pi OS (/etc/xdg/labwc/autostart)
+        # Note, both displays are forced on with "...@30e" kernel modes above
+        kanshi_config_path = Path.home() / ".config/kanshi/config"
+        config_files[kanshi_config_path] = textwrap.dedent("""
+            profile {
+                output "JZI Beetronics bee0097-73092" position 0,0
+                output "JZI BEETRONICS bee0097-63090" position 1024,0
+            }
+        """).lstrip()
+
+    for path, contents in config_files.items():
+        logging.info("⚙️ Checking %s", path)
         if not (path.is_file() and path.read_text() == contents):
-            sub.run("sudo", "tee", path, input=contents, encoding="utf8")
-            udev_restart_required |= path_str.startswith("/etc/udev")
+            try:
+                path.parent.mkdir(parents=True, exist_ok=True)
+                path.write_text(contents)
+                logging.info("✏️ Updated %s", path)
+            except PermissionError:
+                if not path.parent.is_dir():
+                    sub.run("sudo", "mkdir", "-p", path.parent)
+                sub.run("sudo", "tee", path, input=contents, encoding="utf8")
+
+            udev_restart_required |= Path("/etc/udev") in path.parents
 
     if udev_restart_required:
         sub.run("sudo", "udevadm", "control", "--reload-rules")
@@ -132,9 +123,7 @@ def main(bot, base, testbed):
 
     if service_name:
         source_unit_path = REPO_PATH / service_name
-        user_unit_path = (
-            pathlib.Path.home() / ".config/systemd/user" / service_name
-        )
+        user_unit_path = Path.home() / ".config/systemd/user" / service_name
 
         logging.info("⚙️ Checking user systemd unit: %s", service_name)
         if user_unit_path.resolve() != source_unit_path:
